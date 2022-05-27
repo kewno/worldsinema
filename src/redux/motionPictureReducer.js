a4a8073f1ebacfb1e66cac7f33b2e8bf386cd94a
kkewno <kkewno@mail.ru>
2022-05-27 15:08:43 +0300

    session not work

--- src/redux/motionPictureReducer.js
+++ src/redux/motionPictureReducer.js
@@ -1,4 +1,4 @@
-import {motionPictureAPI} from "../api/api";
+import {motionPictureAPI, userAPI} from "../api/api";
 
 const SET_ACTIVE_CINEMA = 'SET_ACTIVE_CINEMA';
 const SET_ACTIVE_GENRE = 'SET_ACTIVE_GENRE';
@@ -9,6 +9,8 @@ const SET_PICTURE = 'SET_PICTURE';
 const SET_COUNT_PICTURE = 'SET_COUNT_PICTURE';
 const SET_ACTIVE_PAGE = 'SET_ACTIVE_PAGE';
 const SET_COMMENTS = 'SET_COMMENTS';
+const SET_AUTH = 'SET_AUTH';
+const DELL_AUTH = 'DELL_AUTH';
 
 export const setActiveCinema = (picture) => {
     return {type: SET_ACTIVE_CINEMA, picture};
@@ -44,6 +46,13 @@ export const setActivePage = (countPage) => {
 
 export const setComments = (comments) => {
     return {type: SET_COMMENTS, comments}; // tyt getPicture
+}
+
+export const setAuth = (auth) => {
+    return {type: SET_AUTH, auth};
+}
+export const dellAuth = (auth) => {
+    return {type: DELL_AUTH, auth};
 }
 
 export const setGenresThunkCreator = () => {
@@ -106,6 +115,35 @@ export const deleteFavoriteThunkCreator = (idCinema, idUser) => {
     }
 }
 
+export const isAuthThunkCreator = () => {
+    return async (dispatch) => {
+        let response = await userAPI.isAuth()
+        //debugger
+        dispatch(setAuth(response.items));
+    }
+}
+
+export const dellAuthThunkCreator = () => {
+    return async (dispatch) => {
+        let response = await userAPI.logout(1)
+        dispatch(dellAuth()); 
+    }
+}
+
+export const registrationThunkCreator = (data) => {
+    return async (dispatch) => {
+        let response = await userAPI.registration(data.name, data.surname, '2022-06-05', data.login, data.password, data.gender)
+    }
+}
+
+export const loginThunkCreator = (data) => { //login(email, password)
+    return async (dispatch) => {
+        let response = await userAPI.login(data.login, data.password)
+        //debugger
+        dispatch(isAuthThunkCreator())
+    }
+}
+
 // export const addCommentsForPictureThunkCreator = (message, idUser, idSinema) => { //comments
 //     return async (dispatch) => { postFavoriteForPicture(idCinema, idUser)
 //         let response = await motionPictureAPI.getCommentsForPicture(message, idUser, idSinema)
@@ -124,7 +162,8 @@ let initMotionPicture = {
     countPicture: 1,
     activeCinema: {id: 1, name: 'Во все тяжкие1', path: 'pyt 1', poster: 'poster 1', country: 'США1', rating: '11', description: '123 Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ducimus sequi deserunt. Molestias veniam, cupiditate placeat explicabo quisquam voluptas et culpa, repellat esse quidem iure blanditiis', years: '2007-2016', producer: 'Винс гиллиган', tags : ['Криминальная драма', 'Черная комедия'], favourites: false},//this.motionData.length
     genre: [{name: 'Триллер'}],
-    comments: []
+    comments: [],
+    auth: {}
 }
 
 let motionPictureReducer = (state = initMotionPicture, action) => {
@@ -157,6 +196,15 @@ let motionPictureReducer = (state = initMotionPicture, action) => {
     } else if (action.type === SET_COMMENTS) {
         stateClone.comments = [...state.comments]
         stateClone.comments = action.comments
+    } else if (action.type === SET_AUTH) {
+        stateClone.auth = {...state.auth}
+        //debugger
+        if (action.auth) stateClone.auth = action.auth
+            else  stateClone.auth = {}
+    } else if (action.type === DELL_AUTH) {
+        stateClone.auth = {...state.auth}
+        //debugger
+        stateClone.auth = {}
     }
     return stateClone;
 }
